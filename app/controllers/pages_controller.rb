c695dfe4dbc0e22f90870baa2c3db84bbc18cff6
codecademy <ccuser@codecademy.com>
2024-10-21 22:22:03 +0000

    adding about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,6 @@ class PagesController < ApplicationController
   end
   def portfolio
   end
-  
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 root 'pages#welcome'
 get '/portfolio', to: 'pages#portfolio', as: :portfolio
+get '/about', to: 'pages#about', as: :about
   # Defines the root path route ("/")
   # root "articles#index"
 end
